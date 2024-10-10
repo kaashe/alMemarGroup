ecd7a41928de4d3ae4c36a8b44c44d5bd1c3e912
Kashif Ullah <43569363+kaashe@users.noreply.github.com>
2024-10-11 03:35:55 +0500

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import { BiSolidSun, BiSolidMoon } from "react-icons/bi";
+import { HiMenuAlt3, HiMenuAlt1 } from "react-icons/hi";
+import ResponsiveMenu from "./ResponsiveMenu";
+
+export const Navlinks = [
+  {
+    id: 1,
+    name: "HOME",
+    link: "/#",
+  },
+  {
+    id: 3,
+    name: "ABOUT",
+    link: "/#about",
+  },
+  {
+    id: 4,
+    name: "BOOKING",
+    link: "/#booking",
+  },
+];
+
+const Navbar = ({ theme, setTheme }) => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <div className="relative z-10 shadow-md w-full dark:bg-black dark:text-white duration-300">
+      <div className="container py-2 md:py-0">
+        <div className="flex justify-between items-center">
+          {/* Brand Name */}
+          <div>
+            <span className="text-2xl font-extrabold font-serif tracking-wide text-primary">
+            Al Memar Trading & Cont Co
+            </span>
+          </div>
+          {/* Desktop Navbar */}
+          <nav className="hidden md:block">
+            <ul className="flex items-center gap-8">
+              {Navlinks.map(({ id, name, link }) => (
+                <li key={id} className="py-4">
+                  <a
+                    href={link}
+                    className="text-lg font-semibold font-sans hover:text-primary transition duration-300 hover:border-b-2 hover:border-primary tracking-wide"
+                  >
+                    {name}
+                  </a>
+                </li>
+              ))}
+              {/* Dark Mode Toggle */}
+              {theme === "dark" ? (
+                <BiSolidSun
+                  onClick={() => setTheme("light")}
+                  className="text-2xl cursor-pointer"
+                />
+              ) : (
+                <BiSolidMoon
+                  onClick={() => setTheme("dark")}
+                  className="text-2xl cursor-pointer"
+                />
+              )}
+            </ul>
+          </nav>
+          {/* Mobile Menu Toggle */}
+          <div className="flex items-center gap-4 md:hidden">
+            {/* Dark Mode Toggle for Mobile */}
+            {theme === "dark" ? (
+              <BiSolidSun
+                onClick={() => setTheme("light")}
+                className="text-2xl cursor-pointer"
+              />
+            ) : (
+              <BiSolidMoon
+                onClick={() => setTheme("dark")}
+                className="text-2xl cursor-pointer"
+              />
+            )}
+            {/* Mobile Menu Icon */}
+            {showMenu ? (
+              <HiMenuAlt1
+                onClick={toggleMenu}
+                className="cursor-pointer transition-all"
+                size={30}
+              />
+            ) : (
+              <HiMenuAlt3
+                onClick={toggleMenu}
+                className="cursor-pointer transition-all"
+                size={30}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+      <ResponsiveMenu showMenu={showMenu} />
+    </div>
+  );
+};
+
+export default Navbar;
