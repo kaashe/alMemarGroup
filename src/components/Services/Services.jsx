e928ff30fab34b4755481d61e67a96691f3b6bad
Kashif Ullah <43569363+kaashe@users.noreply.github.com>
2024-11-02 01:05:29 +0500

    added services

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,31 +1,73 @@
 import React from "react";
-import { FaCameraRetro } from "react-icons/fa";
+import { MdConstruction } from "react-icons/md";
+import { FaTruckRampBox } from "react-icons/fa6";
+import { GiHumanPyramid } from "react-icons/gi";
+import { GiFloorHatch } from "react-icons/gi";
+import { FaHandHoldingWater } from "react-icons/fa";
+import { MdGridOn } from "react-icons/md";
+
 import { GiNotebook } from "react-icons/gi";
 import { SlNote } from "react-icons/sl";
 
 const skillsData = [
   {
-    name: "Reliable Machinery",
+    name: "Construction Devision",
     icon: (
-      <FaCameraRetro className="text-5xl text-primary group-hover:text-black duration-300" />
+      <MdConstruction className="text-5xl text-primary group-hover:text-black duration-300" />
     ),
     link: "#",
-    description: "Built to perform under the toughest conditions, our reliable machinery ensures maximum productivity and durability on every job site.",
+    description: "Construction Division specializes in delivering comprehensive construction solutions, from project planning and design to execution and management. With a focus on quality, safety, and efficiency, we ensure each project is built to the highest standards",
     aosDelay: "0",
   },
   {
-    name: "Safe and Efficient Operations",
+    name: "Procurement Division",
     icon: (
       <GiNotebook className="text-5xl text-primary group-hover:text-black duration-300" />
     ),
     link: "#",
-    description: "Committed to safety and efficiency, our operations are designed to minimize risks while maximizing productivity on every project.",
+    description: "Procurement Division streamlines the sourcing and acquisition of materials and equipment, ensuring timely and cost-effective support for all construction projects. Our team leverages a strong supplier network to maintain high standards and budget control across each phase of procurement.",
     aosDelay: "500",
   },
   {
-    name: "Experience Drivers",
+    name: " Excavation Work",
     icon: (
-      <SlNote className="text-5xl text-primary group-hover:text-black duration-500" />
+      <FaTruckRampBox className="text-5xl text-primary group-hover:text-black duration-500" />
+    ),
+    link: "#",
+    description:"Almemar Group provides specialized excavation services, preparing sites with precision for foundations, utilities, and structural work. Our experienced team and advanced machinery ensure safe, efficient, and thorough excavation for all types of projects",
+    aosDelay: "1000",
+  },
+  {
+    name: "Man Power supply",
+    icon: (
+      <GiHumanPyramid className="text-5xl text-primary group-hover:text-black duration-500" />
+    ),
+    link: "#",
+    description:"Offers reliable manpower supply services, providing skilled and qualified personnel to meet the labor demands of construction and industrial projects. Our team ensures efficient workforce solutions tailored to project requirements.",
+    aosDelay: "1000",
+  },
+  {
+    name: "Floor Coatings Specialist",
+    icon: (
+      <GiFloorHatch className="text-5xl text-primary group-hover:text-black duration-500" />
+    ),
+    link: "#",
+    description:"Provides expert floor coating services, delivering durable and high-quality finishes for industrial, commercial, and residential floors. Our coatings enhance surface protection, durability, and aesthetics to meet project-specific needs.",
+    aosDelay: "1000",
+  },
+  {
+    name: "Water Proofing Specialist",
+    icon: (
+      <FaHandHoldingWater className="text-5xl text-primary group-hover:text-black duration-500" />
+    ),
+    link: "#",
+    description:"Offers professional waterproofing services, protecting structures from water damage and ensuring longevity. Our expertise covers a range of solutions for roofs, basements, and other vulnerable areas, tailored to withstand harsh conditions.",
+    aosDelay: "1000",
+  },
+  {
+    name: "Interlock Brick Pavers",
+    icon: (
+      <MdGridOn className="text-5xl text-primary group-hover:text-black duration-500" />
     ),
     link: "#",
     description:"Our team of experienced drivers brings skill and precision to every project, ensuring safe and timely delivery of results.",
@@ -47,7 +89,7 @@ const Services = () => {
             </h1>
           </div>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-            {skillsData.map((skill) => (
+            {skillsData?.map((skill) => (
               <div
                 key={skill.name}
                 data-aos="fade-up"
