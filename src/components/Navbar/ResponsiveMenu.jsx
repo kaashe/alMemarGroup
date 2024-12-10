897e3cd128d9ef92f36a15ce658b1f5e64471291
Kashif Ullah <kashifullah.dev@gmail.com>
2024-12-10 15:57:11 +0500

    mobile menu translation done

--- src/components/Navbar/ResponsiveMenu.jsx
+++ src/components/Navbar/ResponsiveMenu.jsx
@@ -1,27 +1,23 @@
 import React from "react";
-import { FaUserCircle } from "react-icons/fa";
-
-import { Navlinks } from "./Navbar";
+import { useTranslation } from "react-i18next";
 
 const ResponsiveMenu = ({ showMenu }) => {
-  console.log("showMenu", showMenu);
+  const { t } = useTranslation();
+
   return (
     <div
       className={`${showMenu ? "left-0" : "-left-[100%]"
         } fixed bottom-0 top-0 z-20 flex h-screen w-[75%] flex-col justify-between bg-white dark:bg-gray-900 dark:text-white px-8 pb-6 pt-16 text-black transition-all duration-200 md:hidden rounded-r-xl shadow-md`}
     >
       <div className="card">
-        <div className="flex items-center justify-start gap-3">
-          {/* <FaUserCircle size={50} /> */}
-          {/* <div>
-            <h1>Hello User</h1>
-            <h1 className="text-sm text-slate-500">Premium user</h1>
-          </div> */}
-        </div>
         <nav className="mt-12">
           <ul className="space-y-4 text-xl">
-            {Navlinks.map((data) => (
-              <li key={data?.id}>
+            {[
+              { id: 1, name: t("navbar.Home"), link: "/#" },
+              { id: 3, name: t("navbar.About"), link: "/#about" },
+              { id: 4, name: t("navbar.Bookings"), link: "/#booking" },
+            ].map((data) => (
+              <li key={data.id}>
                 <a href={data.link} className="mb-5 inline-block font-sans">
                   {data.name}
                 </a>
@@ -32,7 +28,8 @@ const ResponsiveMenu = ({ showMenu }) => {
       </div>
       <div className="footer">
         <h1>
-          Made with ❤ by <a href="https://wolves-pack-4yv8.vercel.app/">WolvesPack</a>{" "}
+          Made with ❤ by{" "}
+          <a href="https://wolves-pack-4yv8.vercel.app/">WolvesPack</a>
         </h1>
       </div>
     </div>
